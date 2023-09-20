17da75efed8b540c74992abe56dbd21609b11c6d
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-09-20 13:34:10 -0500

    removed dayjs console.log, and updated html file with hours and formatting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log(dayjs());
+
 $(document).ready(function () {
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
