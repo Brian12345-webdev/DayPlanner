f05802640aef674efb7fb303a1e78b6ffbc3ccca
Brian12345-webdev <132722257+Brian12345-webdev@users.noreply.github.com>
2023-09-25 16:56:19 -0500

    update to reset button, reset to local storage

--- script.js
+++ script.js
@@ -2,18 +2,13 @@ const localeSettings = {};
 dayjs.locale(localeSettings);
 
 function updateTime() {
-
-  var resetScheduler = document.getElementById('.reset-btn');
-  $(".reset-btn").on('click', function () {
-    resetScheduler.removeChildren
+// reset button state
+  $("#reset-btn").on('click', function () {
+    localStorage.clear();
   });
-
-
-
 
   var currentDateTime = dayjs().format('MMM DD, YYYY [at] hh:mm:ss');
   $('#current-dateTime').html(currentDateTime);
-
 }
 setInterval(updateTime, 1000);
 
@@ -25,8 +20,7 @@ $(function () {
   function colorByHour() {
     $('.time-block').each(function () {
       const blockHour = parseInt(this.id.split('-')[1]);
-      console.log(blockHour);
-      console.log(parseInt(currentHour));
+      
       if (blockHour < parseInt(currentHour)) {
         $(this).addClass('past');
       } else if (blockHour === parseInt(currentHour)) {
@@ -51,16 +45,6 @@ $(function () {
     $(this).children('.description').val(value);
   });
 
-  function updateTime() {
-    const currentDate = dayjs().format('dddd, MMMM D, YYYY');
-    const currentTime = dayjs().format('hh:mm:ss A');
-    const date = $('.date');
-    const time = $('.time');
-    date.text(date);
-    time.text(time);
-    $('.date').text(currentDate);
-    $('.time').text(currentTime);
-  }
 
   colorByHour();
   enterText();
